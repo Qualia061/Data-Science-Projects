25c3b97043a7932394541aecf49717a82320c569
Qualia061 <35321011+Qualia061@users.noreply.github.com>
2019-01-21 23:12:41 +0800

    123

--- Python Web Scraping/Hurricane Paths/hurricane_data.py
+++ Python Web Scraping/Hurricane Paths/hurricane_data.py
@@ -10,14 +10,3 @@ HOME_PAGE_URL = "https://www.buzzfeednews.com/"
 driver = webdriver.Chrome('./chromedriver')
 driver.get(HOME_PAGE_URL)
 
-while True:
-    try:
-        loadMoreButton = driver.find_element_by_xpath('//*[@id="mod-show-more-1"]/div/button')
-        time.sleep(2)
-        loadMoreButton.click()
-        time.sleep(5)
-    except Exception as e:
-        print (e)
-        break
-    
-print ("Complete")
